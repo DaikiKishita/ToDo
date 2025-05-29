50ab7e51f41788d6c12c0b2072f522815a862b12
DaikiKishita <daikik167@gmail.com>
2025-05-29 12:43:44 +0900

    Taskクラス作成

---
+++ sample-servlet/src/main/java/com/example/Task.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+public class Task{
+    private String name;
+    private String description;
+    private boolean completed;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+}

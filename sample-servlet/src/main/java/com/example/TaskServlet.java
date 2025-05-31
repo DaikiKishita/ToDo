925e443100e00071967da4743e358a9638fd4b55
DaikiKishita <daikik167@gmail.com>
2025-05-31 16:51:58 +0900

    ログイン後にヘッダーが変更されるよう②

--- sample-servlet/src/main/java/com/example/TaskServlet.java
+++ sample-servlet/src/main/java/com/example/TaskServlet.java
@@ -14,7 +14,7 @@ public class TaskServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
         User user = (User) session.getAttribute("user");
 
         if (user == null) {
